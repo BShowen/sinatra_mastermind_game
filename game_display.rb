aebdb7125d158b37df79a771c546f1d86be10f6a
Bradley Showen <BShowen@me.com>
2019-03-17 18:56:08 -0400

    make the display text more legible

--- game_display.rb
+++ game_display.rb
@@ -1,8 +1,15 @@
 module GameDisplay
     def intro
-        puts "Welcome to Mastermind!\nThe object of the game is to guess a secret code that the computer has generated.\nThe code is only 4 digits long and consists only of numbers. You will have 6 attempts to break the code.\nIf you are unable to guess the code in under 6 attempts then you lose\nThe computer will provide feedback to you after each guess."
-        puts "A #{"red".colorize(:red)} number indicates the number is a correct guess in the correct spot.\nA #{"green".colorize(:green)} number indicates a correct guess in the wrong spot\nA #{"blue".colorize(:light_cyan)} color indicates a wrong guess."
-        puts "For example, if the secret code is 1234 and your guess is 5261 you would see \n#{5.to_s.colorize(:light_cyan)}#{2.to_s.colorize(:red)}#{6.to_s.colorize(:light_cyan)}#{1.to_s.colorize(:green)}"
+        puts `clear`
+        puts "Welcome to Mastermind!\n".center(100) 
+        puts "\n\t 1) The object of the game is to guess a 4 digit secret code that the computer has generated.\n"
+        puts "\n\t 2) You only have 6 attempts to break the code.\n"
+        puts "\n\t 3) The computer will provide feedback to you after each guess.\n"
+        puts "\n\t#{"HINTS".colorize(:light_magenta)}:"
+        puts "\t\tA #{"red".colorize(:red)} number indicates the number is a correct guess in the correct spot."
+        puts "\t\tA #{"green".colorize(:green)} number indicates a correct guess in the wrong spot."
+        puts "\t\tA #{"blue".colorize(:light_cyan)} number indicates a wrong guess.\n"
+        puts "\n\t #{"For example:".colorize(:light_cyan)} The secret code is 1234. Your guess is 5261. You would see #{5.to_s.colorize(:light_cyan)}#{2.to_s.colorize(:red)}#{6.to_s.colorize(:light_cyan)}#{1.to_s.colorize(:green)}\n"
     end
 
 end
