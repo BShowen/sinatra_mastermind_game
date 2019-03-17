52acb679009bfdca2bcfddb42c2183caf692123f
Bradley Showen <BShowen@me.com>
2019-03-17 10:50:22 -0400

    implement guessing feature
    initial implementation the guessing feature. Very primitive at this point. Obviously needs to be refactored

--- computer.rb
+++ computer.rb
@@ -7,9 +7,21 @@ class Computer
 
     def generate_random_code
         until @code.length == 4
-            random_number = rand(6)+1
+            random_number = rand(9)+1
             @code << random_number unless @code.include? random_number
         end
     end
+
+    def attempt_a_crack players_guess
+        results = @code.each do |num|
+            puts "index"
+            if players_guess.include? num
+                puts "correct guess and location" if @code.index(num) == players_guess.index(num)
+                puts "correct guess wrong location" if @code.index(num) != players_guess.index(num)
+            else
+                puts "wrong guess"
+            end
+        end
+    end 
 
 end
