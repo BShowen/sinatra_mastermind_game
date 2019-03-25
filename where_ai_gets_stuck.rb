c4c4e457ce2ea754e21b026d23878308c6874373
Bradley Showen <BShowen@me.com>
2019-03-25 17:24:47 -0400

    update my notes

--- where_ai_gets_stuck.rb
+++ where_ai_gets_stuck.rb
@@ -1,3 +1,6 @@
+# Disregard this file. These are just notes for myself. 
+#Ai gets stuck
+
 # What is your secret code?
 # 4857
 # thinking...
@@ -5,11 +8,11 @@
 # thinking...
 # ai guess is [5, 7, 4, 0]
 # thinking...
-# ai guess is [7, 5, 8, 4]
+# ai guess is [7, 5, 8, 4]              **Ai has all the correct numbers in this guess**
 # thinking...
-# ai guess is [8, 4, 7, 9]
+# ai guess is [8, 4, 7, 9]              **So why did AI get rid of 5 and replace it with 9?**
 # thinking...
-# ai guess is [4, 8, 6, 7]
+# ai guess is [4, 8, 6, 7]              **Crashed**
 
 # 1345
 # thinking...
@@ -17,17 +20,17 @@
 # thinking...
 # ai guess is [0, 4, 7, 2]
 # thinking...
-# ai guess is [5, 1, 4, 3]
+# ai guess is [5, 1, 4, 3]          **Crashed**
 
 # What is your secret code?
 # 1234
 # Let me think..
 # AI guess is 9156
 # Let me think..
-# AI guess is 1423
+# AI guess is 1423              **AI had all the correct numbers inthis guess**
 # Let me think..
-# AI guess is 1240
+# AI guess is 1240              **AI should not have removed 3 to add 0**
 # Let me think..
 # AI guess is 1274
 # Let me think..
-# AI guess is 1284
+# AI guess is 1284              **Crashed**
